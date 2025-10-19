66970d16e3fec9c18b1b327672498856e1bbdbf3
arjunkkhadka <arjun.khadka@sands.com>
2025-10-19 16:24:45 -0500

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import MarketingApp from './MarketingApp';
+import React from 'react';
+export default () => {
+  return (
+    <div>
+      <h1>Container App</h1>
+      <MarketingApp />
+    </div>
+  );
+};
